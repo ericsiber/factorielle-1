a4ee77ceb54be2d30fa8aac25ecd2eeb288e9c0b
Eric Siber <eric.siber@gmail.com>
2017-09-26 17:19:04 +0200

    Première partie étape 6

--- src/test/java/co/simplon/factorielle/FactorielleTest.java
+++ src/test/java/co/simplon/factorielle/FactorielleTest.java
@@ -2,11 +2,13 @@ package co.simplon.factorielle;
 
 import static org.junit.Assert.*;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class FactorielleTest {
 
 	@Test
+	@Ignore
 	public void factorielle_de_0_doit_renvoyer_1() {
 		// GIVEN
 		long n = 0;
