7a802589265e0f8a565fb0398223b63cbcba79eb
Eric Siber <eric.siber@gmail.com>
2017-09-26 17:09:59 +0200

    Etape 5

--- src/test/java/co/simplon/factorielle/FactorielleTest.java
+++ src/test/java/co/simplon/factorielle/FactorielleTest.java
@@ -7,6 +7,19 @@ import org.junit.Test;
 public class FactorielleTest {
 
 	@Test
+	public void factorielle_de_0_doit_renvoyer_1() {
+		// GIVEN
+		long n = 0;
+		Factorielle factorielle = new Factorielle();
+		
+		// WHEN
+		long resultat = factorielle.calculer(n);
+		
+		// THEN
+		assertEquals(1, resultat);
+	}
+	
+	@Test
 	public void factorielle_de_1_doit_renvoyer_1() {
 		// GIVEN
 		long n = 1;
@@ -17,6 +30,45 @@ public class FactorielleTest {
 		
 		// THEN
 		assertEquals(1, resultat);
+	}
+	
+	@Test
+	public void factorielle_de_2_doit_renvoyer_2() {
+		// GIVEN
+		long n = 2;
+		Factorielle factorielle = new Factorielle();
+		
+		// WHEN
+		long resultat = factorielle.calculer(n);
+		
+		// THEN
+		assertEquals(2, resultat);
+	}
+	
+	@Test
+	public void factorielle_de_3_doit_renvoyer_6() {
+		// GIVEN
+		long n = 3;
+		Factorielle factorielle = new Factorielle();
+		
+		// WHEN
+		long resultat = factorielle.calculer(n);
+		
+		// THEN
+		assertEquals(6, resultat);
+	}
+
+	@Test
+	public void factorielle_de_19_doit_renvoyer_121645100408832000() {
+		// GIVEN
+		long n = 19;
+		Factorielle factorielle = new Factorielle();
+		
+		// WHEN
+		long resultat = factorielle.calculer(n);
+		
+		// THEN
+		assertEquals(121645100408832000l, resultat);
 	}
 
 }
