87d0e07d6138d205578bb9324180dc97fa0a176f
Eric Siber <eric.siber@gmail.com>
2017-09-29 15:40:26 +0200

    étapes 4 et 5

--- src/main/java/co/simplon/factorielle/Factorielle.java
+++ src/main/java/co/simplon/factorielle/Factorielle.java
@@ -1,26 +1,31 @@
 package co.simplon.factorielle;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Factorielle
  */
 public class Factorielle 
 {
+	private static Logger LOGGER = LoggerFactory.getLogger(Factorielle.class);
+	
     public Factorielle() {
     		super();
     }
     
     public long calculer(long n) {
-    		System.out.println("Appel de la méthode calculer avec le paramètre " + n);
+    		LOGGER.debug("Appel de la méthode calculer avec le paramètre " + n);
     		long resultat;
     		if (n > 1) {
     			resultat = n * calculer(n-1);
     		} else if (n < 0) {
-    			System.err.println("Cas d'erreur détecté, entier négatif non supporté : " + n);
+    			LOGGER.error("Cas d'erreur détecté, entier négatif non supporté : " + n);
     			throw new IllegalArgumentException();
     		} else {
     			resultat = 1;
     		}
-    		System.out.println("Résultat de l'appel avec " + n + " = " + resultat);
+    		LOGGER.info("Résultat de l'appel avec " + n + " = " + resultat);
     		return resultat;
     }
 
