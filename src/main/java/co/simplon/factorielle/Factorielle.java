d08c6fc4ef1079faa94daac5a2acaede44de1c21
Eric Siber <eric.siber@gmail.com>
2017-09-29 15:01:57 +0200

    Logging étape 3

--- src/main/java/co/simplon/factorielle/Factorielle.java
+++ src/main/java/co/simplon/factorielle/Factorielle.java
@@ -10,12 +10,18 @@ public class Factorielle
     }
     
     public long calculer(long n) {
+    		System.out.println("Appel de la méthode calculer avec le paramètre " + n);
+    		long resultat;
     		if (n > 1) {
-    			return n * calculer(n-1);
+    			resultat = n * calculer(n-1);
     		} else if (n < 0) {
+    			System.err.println("Cas d'erreur détecté, entier négatif non supporté : " + n);
     			throw new IllegalArgumentException();
+    		} else {
+    			resultat = 1;
     		}
-    		return 1;
+    		System.out.println("Résultat de l'appel avec " + n + " = " + resultat);
+    		return resultat;
     }
 
 }
