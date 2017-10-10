21130d4837c13cebabcd47b0776f76432c1fe203
Eric Siber <eric.siber@gmail.com>
2017-10-10 13:19:16 +0200

    autres activités étape 6

--- src/test/java/co/simplon/factorielle/FactorielleTest.java
+++ src/test/java/co/simplon/factorielle/FactorielleTest.java
@@ -2,60 +2,66 @@ package co.simplon.factorielle;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 
 public class FactorielleTest {
+
+	Factorielle factorielle = null;
+
+	@Before
+	public void setupFactorielle() {
+		factorielle = new Factorielle();
+	}
 
 	@Test
 	@Ignore
 	public void factorielle_de_0_doit_renvoyer_1() {
 		// GIVEN
 		long n = 0;
-		Factorielle factorielle = new Factorielle();
-		
+
 		// WHEN
 		long resultat = factorielle.calculer(n);
-		
+
 		// THEN
 		assertEquals(1, resultat);
 	}
-	
+
 	@Test
 	public void factorielle_de_1_doit_renvoyer_1() {
 		// GIVEN
 		long n = 1;
-		Factorielle factorielle = new Factorielle();
-		
+
 		// WHEN
 		long resultat = factorielle.calculer(n);
-		
+
 		// THEN
-		assertEquals(1, resultat);
+		assertEquals("Erreur sur le calcul de factorielle(1)", 1, resultat);
 	}
-	
+
 	@Test
 	public void factorielle_de_2_doit_renvoyer_2() {
 		// GIVEN
 		long n = 2;
-		Factorielle factorielle = new Factorielle();
-		
+
 		// WHEN
 		long resultat = factorielle.calculer(n);
-		
+
 		// THEN
-		assertEquals(2, resultat);
+		// mauvaise pratique, privilégier assertEquals pour garder l'information
+		// valeur attendue / valeur obtenue
+		assertTrue(resultat == 2);
 	}
-	
+
 	@Test
 	public void factorielle_de_3_doit_renvoyer_6() {
 		// GIVEN
 		long n = 3;
-		Factorielle factorielle = new Factorielle();
-		
+
 		// WHEN
 		long resultat = factorielle.calculer(n);
-		
+
 		// THEN
 		assertEquals(6, resultat);
 	}
@@ -64,11 +70,10 @@ public class FactorielleTest {
 	public void factorielle_de_19_doit_renvoyer_121645100408832000() {
 		// GIVEN
 		long n = 19;
-		Factorielle factorielle = new Factorielle();
-		
+
 		// WHEN
 		long resultat = factorielle.calculer(n);
-		
+
 		// THEN
 		assertEquals(121645100408832000l, resultat);
 	}
